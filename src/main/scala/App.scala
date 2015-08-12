c4a680c3900aaf2526fe2400cd9a6c0b8e1344e2
Miłosz Jaśniok <milosz.jasniok@xsolve.pl>
2015-08-12 16:41:29 +0200

    Ugly change to scala language

---
+++ src/main/scala/App.scala
@@ -0,0 +1,45 @@
+import java.io.ByteArrayOutputStream
+import javafx.application.Application
+
+import org.apache.log4j.Logger
+import org.kohsuke.args4j.{CmdLineException, CmdLineParser}
+import pl.oen.msi.keyboard.{Args, DeviceNotFoundException, KeyboardUsbConnector, MainWindow}
+
+import scala.collection.JavaConversions._
+
+object App {
+  val LOGGER: Logger = Logger.getLogger(classOf[App])
+
+  def main(args: Array[String]): Unit = {
+
+    val args2: Args = new Args
+    val parser: CmdLineParser = new CmdLineParser(args2)
+
+    try {
+      parser.parseArgument(args.toSeq)
+      if (args2.x) {
+        startConsoleApp(args2)
+      } else {
+        Application.launch(classOf[MainWindow], args:_*)
+      }
+    } catch {
+      case e: CmdLineException =>
+        val error: String = e.getMessage + System.lineSeparator
+        val baos: ByteArrayOutputStream = new ByteArrayOutputStream
+        parser.printUsage(baos)
+        LOGGER.error(error + baos.toString)
+    }
+  }
+
+  def startConsoleApp(args: Args) {
+    val keyboardUsbConnector: KeyboardUsbConnector = new KeyboardUsbConnector
+    try {
+      keyboardUsbConnector.setColours(args.colour1, args.colour2, args.colour3)
+    } catch {
+      case e: DeviceNotFoundException => {
+        LOGGER.error("Device not found")
+        LOGGER.debug("Device not found", e)
+      }
+    }
+  }
+}
